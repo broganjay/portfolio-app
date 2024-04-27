d14339b0b620e3e1210898d7c42e67c0199b5651
bji1 <bji1@st-andrews.ac.uk>
2024-04-27 17:29:27 +0100

    Remove apostrophe... must be escaped

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,7 @@ export default function Contact() {
     return (
         <>
             <h1>Contact</h1>
-            <p>Here's how to contact me...</p>
+            <p>Here is how to contact me...</p>
         </>
     );
 
