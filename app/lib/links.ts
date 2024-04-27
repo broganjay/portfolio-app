2a39039a90ab6f5c823facf283fce330c59f3b8e
bji1 <bji1@st-andrews.ac.uk>
2024-04-27 16:43:39 +0100

    Add initial structure

---
+++ app/lib/links.ts
@@ -0,0 +1,20 @@
+import { HomeIcon, UserGroupIcon } from '@heroicons/react/24/solid'
+
+export const links = 
+    [
+        {
+            "name": "Home",
+            "href": "/",
+            "icon": HomeIcon
+        },
+        {
+            "name": "About",
+            "href": "/about",
+            "icon": UserGroupIcon
+        },
+        {
+            "name": "Contact",
+            "href": "/contact",
+            "icon": HomeIcon
+        }
+    ]
